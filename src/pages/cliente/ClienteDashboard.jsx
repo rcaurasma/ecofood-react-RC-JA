9a3706947d21fe8ee1289c381212b36c87a9cc03
rcaurasma <rcaurasma>
2025-07-03 21:17:32 -0400

    Version Final 2

--- src/pages/cliente/ClienteDashboard.jsx
+++ src/pages/cliente/ClienteDashboard.jsx
@@ -1,19 +1,26 @@
+import CerrarSesion from "../../components/CerrarSesion";
 import { useNavigate } from "react-router-dom";
 
 export default function ClienteDashboard() {
   const navigate = useNavigate();
 
+  const handleVerProductos = () => {
+    navigate("/home");
+  };
+
   return (
     <div className="container mt-4">
       <h2>Bienvenido cliente</h2>
-      <div className="text-center mt-3">
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={() => navigate("/home", { replace: true })}
+      <div className="mb-3">
+        <button 
+          className="btn btn-primary me-2"
+          onClick={handleVerProductos}
         >
           Ver productos
         </button>
+      </div>
+      <div className="navbar-nav">
+        <CerrarSesion />
       </div>
     </div>
   );
